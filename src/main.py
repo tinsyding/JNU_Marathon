d96646100fd0cfa912d413559b142b536b2ea62c
tinsy <tinsyding@gmail.com>
2023-11-23 11:01:40 +0800

    11.23

--- src/main.py
+++ src/main.py
@@ -3,12 +3,7 @@ from scraper import run_scraper
 from utils import choose_browser
 
 def main():
-    '''
-    Main function to initialize the WebDriver and run the scraper.
-    - Chooses a browser based on user input.
-    - Initializes the WebDriver for the selected browser.
-    - Calls the run_scraper function to execute the scraping process.
-    '''
+    '''Main function to run the scraper'''
     driver = None
     driver = choose_browser()
     run_scraper(driver)
